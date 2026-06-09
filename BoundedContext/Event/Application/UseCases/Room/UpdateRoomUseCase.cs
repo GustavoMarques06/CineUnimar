9bb66e6ae627e034c9daec212e61a3b6c171cbd1
nik-rocha <nicollasrocha320@gmail.com>
2026-06-08 22:20:37 -0300

    feat: Adicionado verificação de theater na criação de room

--- BoundedContext/Event/Application/UseCases/Room/UpdateRoomUseCase.cs
+++ BoundedContext/Event/Application/UseCases/Room/UpdateRoomUseCase.cs
@@ -1,18 +1,26 @@
 ﻿using Api_Venda_Ingressos.BoundedContext.Event.Application.DTOs.Request;
 using Api_Venda_Ingressos.BoundedContext.Event.Domain.Interfaces;
+using Api_Venda_Ingressos.BoundedContext.Event.Infrastructure.Repository;
 
 namespace Api_Venda_Ingressos.BoundedContext.Event.Application.UseCases.Room
 {
     public class UpdateRoomUseCase
     {
+        private readonly ITheaterRepository _theaterRepository;
         private readonly IRoomRepository _roomRepository;
-        public UpdateRoomUseCase(IRoomRepository roomRepository)
+        public UpdateRoomUseCase(ITheaterRepository theaterRepository, IRoomRepository roomRepository)
         {
+            _theaterRepository = theaterRepository;
             _roomRepository = roomRepository;
         }
 
         public async Task RunAsync(UpdateRoomRequest updateRoom)
         {
+            var theater = await _theaterRepository.GetByIdAsync(updateRoom.IdTheater);
+
+            if (theater is null)
+                throw new ArgumentException("Teatro não pode ser nulo");
+
             var room = await _roomRepository.GetByIdAsync(updateRoom.Id);
 
             if (room is null)

4f5ffcd4a8c43dea57a9c9c2c0172c0cafab4909
muri <murillop.o06@gmail.com>
2026-06-11 00:38:46 -0300

    feat: Adição de DTO/requests de room event

---
+++ BoundedContext/Event/Application/DTOs/Request/CreateRoomEventRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Api_Venda_Ingressos.BoundedContext.Event.Application.DTOs.Request
+{
+    public class CreateRoomEventRequest
+    {
+        public Guid IdRoom { get; private set; }
+
+        public CreateRoomEventRequest(Guid idRoom)
+        {
+            IdRoom = idRoom;
+        }
+    }
+}
---
+++ BoundedContext/Event/Application/DTOs/Request/UpdateRoomEventRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace Api_Venda_Ingressos.BoundedContext.Event.Application.DTOs.Request
+{
+    public class UpdateRoomEventRequest
+    {
+        public Guid Id { get; private set; }
+        public Guid IdRoom { get; private set; }
+        public bool IsFull { get; private set; }
+
+        public UpdateRoomEventRequest(Guid id, Guid idRoom, bool isFull)
+        {
+            Id = id;
+            IdRoom = idRoom;
+            IsFull = isFull;
+        }
+    }
+}
